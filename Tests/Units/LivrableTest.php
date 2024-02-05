e5b32a2bdf425b05be56c1ad0865727d7ca9dfc2
Babyd12 <mamadcir24@gmail.com>
2024-02-05 14:37:54 +0000

    ajout pour changer de branche

---
+++ Tests/Units/LivrableTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Tests\Units;
+
+use App\Entity\Livrable;
+
+use App\Entity\Apprenant;
+use App\Entity\Competence;
+use App\Entity\Entreprise;
+use App\Entity\Association;
+use App\Entity\LivrableStatu;
+use PHPUnit\Framework\TestCase;
+use App\Entity\LangageDeProgrammation;
+use Doctrine\Common\Collections\Collection;
+
+class LivrableTest extends TestCase
+{
+    private $livrable;
+    protected function setUp(): void
+    {
+        $this->livrable = new Livrable();
+    }
+
+    public function testGetId(): void
+    {
+        $this->assertNull($this->livrable->getId());
+    }
+
+    public function getLienDuLivrable(): void 
+    {   
+        $lientDuLivrable = 'google.com' ;
+        $this->livrable->setLienDuLivrable($lientDuLivrable);
+        $this->assertEquals($lientDuLivrable, $this->livrable->getLienDuLivrable());
+    }
+
+    public function testsetLienDuLivrable(): void
+    {
+        $lienDuLivrable = 'https://dev-t0gt7fxfqc4r13lt.us.auth0.com/api/v2/';
+    $this->livrable->setLienDuLivrable($lienDuLivrable);
+    $this->assertEquals($lienDuLivrable, $this->livrable->getLienDuLivrable());
+    }
+
+}
--- Tests/Units/ProjetTest.php
+++ Tests/Units/ProjetTest.php
@@ -4,9 +4,13 @@ namespace App\Tests\Units;
 
 use App\Entity\Projet;
 
+use App\Entity\Apprenant;
 use App\Entity\Competence;
 use App\Entity\Entreprise;
+use App\Entity\Association;
+use App\Entity\ProjetStatu;
 use PHPUnit\Framework\TestCase;
+use App\Entity\LangageDeProgrammation;
 use Doctrine\Common\Collections\Collection;
 
 class ProjetTest extends TestCase
@@ -35,10 +39,110 @@ class ProjetTest extends TestCase
         $this->assertEquals('description', $this->projet->getDescription());
     }
 
-    public function testgetNombreDeParticipant()
+    public function testgetNombreDeParticipant(): void
     {
         $this->projet->setNombreDeParticipant(30);
         $this->assertEquals('30', $this->projet->getNombreDeParticipant());
+    }
+
+    public function setNombreDeParticipant(): void
+    {
+        $nombreDeParticipant = $this->projet->setNombreDeParticipant(30);
+        $this->assertEquals('30', $this->projet->getNombreDeParticipant());
+    }
+
+    public function testGetAssociation(): void
+    {
+        $association = new Association(); 
+        $this->projet->setAssociation($association);
+
+        $this->assertInstanceOf(Association::class, $this->projet->getAssociation());
+    }
+
+    public function testgetDateLimite(): void
+    {
+         $this->assertNull($this->projet->getDateLimite());
+    }
+
+    public function testSetDateLimite(): void 
+    {
+        $date = new \DateTime('2024-02-05');
+        $this->projet->setDateLimite($date);
+        $this->assertInstanceOf(\DateTimeInterface::class, $this->projet->getDateLimite());
+        $this->assertEquals($date, $this->projet->getDateLimite());
+    }
+
+    public function testSetAssociation(): void
+    {
+        $association = new Association(); 
+        $this->projet->setAssociation($association);
+        $this->assertEquals($association, $this->projet->getAssociation());
+    }
+
+    public function testGetLangageDeProgrammation(): void
+    {
+        $langage = new LangageDeProgrammation(); 
+        $this->projet->addLangageDeProgrammation($langage);
+        $langages = $this->projet->getLangageDeProgrammation();
+
+        $this->assertInstanceOf(Collection::class, $langages);
+        $this->assertCount(1, $langages);
+        $this->assertTrue($langages->contains($langage));
+    }
+
+    public function testAddLangageDeProgrammation(): void
+    {
+        $langage = new LangageDeProgrammation(); 
+        $this->projet->addLangageDeProgrammation($langage);
+        $this->assertTrue($this->projet->getLangageDeProgrammation()->contains($langage));
+    }
+
+    public function testRemoveLangageDeProgrammation(): void
+    {
+        $langage = new LangageDeProgrammation(); 
+        $this->projet->addLangageDeProgrammation($langage);
+        $this->projet->removeLangageDeProgrammation($langage);
+        $this->assertFalse($this->projet->getLangageDeProgrammation()->contains($langage));
+    }
+
+    public function testGetApprenants(): void
+    {
+        $apprenant = new Apprenant(); 
+        $this->projet->addApprenant($apprenant);
+
+        $apprenants = $this->projet->getApprenants();
+
+        $this->assertInstanceOf(Collection::class, $apprenants);
+        $this->assertCount(1, $apprenants);
+        $this->assertTrue($apprenants->contains($apprenant));
+    }
+
+    public function testAddApprenant(): void
+    {
+        $apprenant = new Apprenant();
+        $this->projet->addApprenant($apprenant);
+        $this->assertTrue($this->projet->getApprenants()->contains($apprenant));
+    }
+
+    public function testRemoveApprenant(): void
+    {
+        $apprenant = new Apprenant(); 
+        $this->projet->addApprenant($apprenant);
+        $this->projet->removeApprenant($apprenant);
+        $this->assertFalse($this->projet->getApprenants()->contains($apprenant));
+    }
+
+    public function testGetStatu(): void
+    {
+        $this->projet->setStatu(ProjetStatu::en_attente);
+        $this->assertInstanceOf(ProjetStatu::class, $this->projet->getStatu());
+    }
+
+    public function testSetStatu(): void
+    {  
+        $statu = ProjetStatu::en_attente;
+        $this->projet->setStatu($statu);
+        $this->assertEquals($statu, $this->projet->getStatu());
     }
 
 }
