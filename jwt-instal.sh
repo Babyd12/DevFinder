efe88259b4f8714885993e3bee51bc874d8b4372
Babyd12 <mamadcir24@gmail.com>
2024-02-10 00:48:12 +0000

    testFunctionnel

--- jwt-instal.sh
+++ jwt-instal.sh
@@ -4,11 +4,10 @@
 # composer require lexik/jwt-authentication-bundle
 # composer install
 #Dans cette commande, echo "JWT_PASSPHRASE" génère la passphrase et la passe à la commande openssl via stdin.
-# mkdir config/jwt
+mkdir -m 775 config/jwt
 echo "JWT_PASSPHRASE" | openssl genrsa -out config/jwt/private.pem -aes256 -passout stdin 4096
 
 openssl rsa -pubout -in config/jwt/private.pem -out config/jwt/public.pem -passin pass:JWT_PASSPHRASE
-
 
 
 
