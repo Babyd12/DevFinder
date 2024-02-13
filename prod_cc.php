96a3ff317a462f529367c6b71a10dd30166eba22
Babyd12 <mamadcir24@gmail.com>
2024-02-13 09:19:16 +0000

    cache clear pour la prod

--- prod_cc.php
+++ prod_cc.php
@@ -1,6 +1,15 @@
 <?php
- $symfonyScript = __DIR__ . '/../../bin/console';  // Modifiez le chemin selon votre structure de projet
 
- // Exécutez la commande de cache clear
- $command = 'php ' . $symfonyScript . ' cache:clear --env=prod --no-warmup';
- exec($command, $output, $returnValue);
+$symfonyScript = __DIR__ . '/bin/console'; 
+
+// Exécutez la commande de cache clear
+$command = 'php ' . $symfonyScript . ' cache:clear --env=prod --no-warmup';
+exec($command, $output, $returnValue);
+
+// Affichez la sortie (utile pour le débogage)
+echo implode("\n", $output);
+
+// Gérer le cas d'échec
+if ($returnValue !== 0) {
+    throw new \RuntimeException('La commande cache:clear a échoué avec le code ' . $returnValue);
+}
